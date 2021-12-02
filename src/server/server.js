c588ba2a5302d5031fac861858cc4ca38e01a9c8
sowmiyaEE <sowmiya.skm@gmail.com>
2021-12-02 22:41:48 +0530

    m

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const {Pool} = require('pg');
 const app=express();
-const port=3001;
+const port=process.env.port || 3001;
 app.listen(port,()=>{console.log(`listening on ${port}`);
 });
 const pool = new Pool({
